45ed81755e821c5eb2b78097474645a17cb9987a
artusvranken <artusvranken@gmail.com>
2017-11-14 14:21:24 +0100

    Added a tsconfig and refactored Currency.ts.

--- domain/Currency.ts
+++ domain/Currency.ts
@@ -1,28 +1,47 @@
+export class Currency 
+{
+    private _name : string;
+    private _rateComparedToUSD : number; 
 
-
-export class Currency {
-    private name:string;
-    private rateComparedToUSD:number
-    private currency:Currency;
-
-    constructor(name:string,rateComparedToUSD:number){
-        this.name=name;
-        this.rateComparedToUSD=rateComparedToUSD;
+    constructor(name : string, rateComparedToUSD : number)
+    {
+        this.name = name;
+        this.rateComparedToUSD = rateComparedToUSD;
+    }
+    
+    /**
+    * Get the name of the currency.
+    * @returns {number} The name of the currency.
+    */
+    get name() : string
+    {
+        return this._name;
+    }
+    
+    /**
+    * Set the name of the currency.
+    * @param {string} name - The name to set the currency to.
+    */
+    set name(name : string)
+    {
+        this._name = name;
+    }
+    
+    /**
+    * Set the rate of the currency compared to USD.
+    * @param {number} rate - The rate of the currency compared to USD.
+    */
+    set rateComparedToUSD(rate : number)
+    {
+        this._rateComparedToUSD = rate;
     }
 
-    Currency(currency:Currency){
-        this.currency=currency;
-    }
-
-    getCurrency():Currency{
-        return this.currency;
-    }
-
-    getName():string{
-        return name;
-    }
-
-    getRateComparedToUSD():number{
-        return this.rateComparedToUSD;
+    /**
+    * Get the rate of the currency compared to USD.
+    * @returns {number} The rate of the currency compared to USD.
+    */
+    get rateComparedToUSD() : number
+    {
+        return this._rateComparedToUSD;
     }
 }
