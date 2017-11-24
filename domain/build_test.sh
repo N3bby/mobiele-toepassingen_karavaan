5c705eb2a16b6a142dd43c443257749538999f7e
artusvranken <artusvranken@gmail.com>
2017-11-24 17:47:57 +0100

    Updated build_test script, documentation will be compiled inside the target directory so that we don't clutter our repository with html files.

--- domain/build_test.sh
+++ domain/build_test.sh
@@ -5,11 +5,16 @@
 # Run by "./build_test"
 #
 
-#Compile TypeScript
+# Compile TypeScript
 tsc
 
-#Compile jsdoc
-jsdoc target/*.js -t docs/docdash -d docs
+# Check if  the documentation theme exists.
+if [ ! -d "target/docs/docdash" ]; then
+git clone https://github.com/clenemt/docdash.git ./target/docs/docdash
+fi 
+
+# Compile jsdoc
+jsdoc target/*.js -t ./target/docs/docdash -d ./target/docs
 
 #run tests
 node target/KaravaanTests.js
