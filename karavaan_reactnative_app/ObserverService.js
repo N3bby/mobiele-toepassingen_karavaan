8dc6217533993e5178c45d0cbd7ba2e408a8e670
Razacx <levi.vandenbempt.sjcm@gmail.com>
2017-12-21 21:18:54 +0100

    Made ObserverService work for deleting Trips

--- karavaan_reactnative_app/ObserverService.js
+++ karavaan_reactnative_app/ObserverService.js
@@ -6,14 +6,21 @@ export class ObserverService {
         this._personMapObservers = [];
         this._tripMapObservers = [];
 
-        this._applyObserverable()
+        this._applyObserverables()
     }
 
-    _applyObserverable() {
+    _applyObserverables() {
+
+        this._applyObservablePersonMap();
+        this._applyObservableTripMap();
+
+    }
+
+    //Applies only for set method (we agreed that people wouldn't be removed?)
+    _applyObservablePersonMap() {
 
         let observerService = this; //So we can access 'this' within the lambda
 
-        //personMap
         this._observedService.personMap.set = (id, person) => {
             Map.prototype.set.apply(observerService._observedService.personMap, [id, person]);
             for(let i = 0; i < observerService._personMapObservers.length; i++) {
@@ -21,13 +28,26 @@ export class ObserverService {
             }
         };
 
-        //tripMap
+    }
+
+    //Applies for both set and delete
+    _applyObservableTripMap() {
+
+        let observerService = this; //So we can access 'this' within the lambda
+
         this._observedService.tripMap.set = (id, trip) => {
             Map.prototype.set.apply(observerService._observedService.tripMap, [id, trip]);
             for(let i = 0; i < observerService._tripMapObservers.length; i++) {
                 observerService._tripMapObservers[i](trip);
             }
-        }
+        };
+
+        this._observedService.tripMap.delete = (id) => {
+            Map.prototype.delete.apply(observerService._observedService.tripMap, [id]);
+            for(let i = 0; i < observerService._tripMapObservers.length; i++) {
+                observerService._tripMapObservers[i]();
+            }
+        };
 
     }
 
