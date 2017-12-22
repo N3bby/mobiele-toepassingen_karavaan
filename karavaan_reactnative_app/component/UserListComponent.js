2965fecf430a766960e452994b9d00ca9a63c260
Razacx <levi.vandenbempt.sjcm@gmail.com>
2017-12-22 22:38:11 +0100

    Generified UserListComponent.js

--- karavaan_reactnative_app/component/UserListComponent.js
+++ karavaan_reactnative_app/component/UserListComponent.js
@@ -21,20 +21,27 @@ import '../ServiceWrapper.js';
 export default class UserListComponent extends React.Component {
 
     //Properties
-    // - navigation: Navigation object
-    // - userSrcFunc: Function that gets the array for users -- see HomeComponent for an example
+    // - navigation (object) : Navigation object
+    // - source (Map) : Reference to userMap object. Can't use a function here since values will only be updated upon re-creating the component then.
+    // - observerFunc (function) : Function that registers a callback to the ObserverService. Takes one argument, (component)
 
-    constructor() {
-        super();
-        //TODO: Make the oobserver stuff generic for this class
-        global.observerService.addPersonMapCallback(() => this.forceUpdate() );
+    // - isPicker (bool) : If true, this component will act as a Picker
+    // - pickerFunc (function) : Function to execute when a user has been clicked
+
+    constructor(props) {
+        super(props);
+        this.props.observerFunc(this);
+    }
+
+    executePickerFunc(user) {
+        this.props.pickerFunc(user);
     }
 
     render() {
         return (
             <Content>
-                <List dataArray={this.props.userSrcFunc()} renderRow={(user) =>
-                    <ListItem key={user.userId} button={false} icon>
+                <List dataArray={Array.from(this.props.source.values())} renderRow={(user) =>
+                    <ListItem key={user.userId} button={this.props.isPicker} onPress={() => this.executePickerFunc(user)} icon>
                         <Left>
                             <Icon style={{fontSize: 25}} name="person"/>
                         </Left>
